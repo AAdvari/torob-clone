b2f23a88ac23435f37bc3299662540bedff09b91
aadvari <amiradvari@gmail.com>
2022-07-03 00:35:28 +0430

    send userType beside token in login

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,5 +1,5 @@
 import {Injectable, UnauthorizedException} from "@nestjs/common";
-import { JwtService } from '@nestjs/jwt';
+import {JwtService} from '@nestjs/jwt';
 import {UserService} from "../../user/services/user.service";
 import {LoginRequestDto} from "../dtos/LoginRequest.dto";
 
