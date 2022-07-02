9157fdcf187ee3fa295a8b76fa64a5a891f87f5b
aadvari <amiradvari@gmail.com>
2022-07-03 00:34:30 +0430

    send userType beside token in login

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
             const payload = { userId: user.id };
             return {
                 token: this.jwtService.sign(payload),
-                userType: user.userType
+                user
             }
         }
         else
